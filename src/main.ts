a98c23ebd3da8239a5bc683976e1cc37de7c99a5
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2023-04-25 20:13:26 -0500

    Second commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const logger = new Logger('examen final');
+  
   logger.log(process.env.MONGO_URL);
+
   await app.listen(3000);
   logger.log(' ===========================================================')
   logger.log(`Application has started in [${process.env.NODE_ENV}] Mode `);
